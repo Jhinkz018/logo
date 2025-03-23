2967b36ef4db9767ba43a26e2ef6fdfd01badf92
0xJhinkz <shanecarla04@gmail.com>
2025-03-23 02:33:13 +0000

    ok

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# logo
+dont mind
---
+++ logo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BLUE='\033[1;34m'
+NC='\033[0m'
+
+echo -e "${BLUE}"
+ echo " ______________"
+ echo "|              |"
+ echo "|   0xJhinkz   |"
+ echo "| with 0xSGFAM |"
+ echo "|              |"
+ echo "|______________|"
+ echo ""
+ echo "Follow Telegram : https://t.me/Jebeincorporated"
+ echo -e "${NC}"
